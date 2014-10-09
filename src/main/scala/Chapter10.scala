dc9022e0bf498e638ca0d4c45973416b240a874e
Amit Kumar <akumar2@pandora.com>
2014-10-08 22:53:47 -0700

    chapter 10 WIP

--- src/main/scala/Chapter10.scala
+++ src/main/scala/Chapter10.scala
@@ -7,8 +7,7 @@ object Chapter10 {
   // some rules.
   // Monoid is container for the data type -like a List for example
   trait Monoid[A]{
-    // the operation op should satisfy the associativity i.e.
-    // op(op(a,b),c) = op(a,op(b,c))
+    // the operation op should satisfy the associativity i.e. op(op(a,b),c) = op(a,op(b,c))
     def op(a1:A, a2:A):A
     // zero or identity function should satisfy op(a,zero) == a
     def zero:A
@@ -17,42 +16,43 @@ object Chapter10 {
 
   // EX 10.1
   class IntAdditionMonoid extends Monoid[Int]{
-    // the operation op should satisfy the associativity i.e.
+    // the operation op should satisfy the associativity i.e. op(op(a,b),c) = op(a,op(b,c))
     def op(a1: Int, a2: Int): Int = a1 + a2
     def zero:Int = 0
   }
 
   class IntMultiplicationMonoid extends Monoid[Int]{
-    // the operation op should satisfy the associativity i.e.
+    // the operation op should satisfy the associativity i.e. op(op(a,b),c) = op(a,op(b,c))
     def op(a1: Int, a2: Int): Int = a1 * a2
     def zero:Int = 1
   }
 
   class BooleanOrMonoid extends Monoid[Boolean]{
-    // the operation op should satisfy the associativity i.e.
+    // the operation op should satisfy the associativity i.e. op(op(a,b),c) = op(a,op(b,c))
     def op(a1: Boolean, a2: Boolean): Boolean = a1 || a2
     // zero or identity function should satisfy op(a,zero) == a
     def zero: Boolean = false
   }
 
   class BooleanAndMonoid extends Monoid[Boolean]{
-    // the operation op should satisfy the associativity i.e.
+    // the operation op should satisfy the associativity i.e. op(op(a,b),c) = op(a,op(b,c))
     def op(a1: Boolean, a2: Boolean): Boolean = a1 && a2
     // zero or identity function should satisfy op(a,zero) == a
     def zero: Boolean = true
   }
 
   class EndoMonoid[A] extends Monoid[ A => A]{
-    // the operation op should satisfy the associativity i.e.
+    // the operation op should satisfy the associativity i.e. op(op(a,b),c) = op(a,op(b,c))
     def op(a1: (A) => A, a2: (A) => A): (A) => A = {
       a1.compose(a2)
+     // or a1 andThen a2
     }
     // zero or identity function should satisfy op(a,zero) == a
     def zero= (a:A) => a
   }
 
   class OptionMonoid[A] extends Monoid[Option[A]]{
-    // the operation op should satisfy the associativity i.e.
+    // the operation op should satisfy the associativity i.e. op(op(a,b),c) = op(a,op(b,c))
     def op(a1: Option[A], a2: Option[A]): Option[A] = {
          a1 orElse  a2
     }
@@ -95,10 +95,41 @@ object Chapter10 {
     val optMonoid = new OptionMonoid[Int]
     require(optMonoid.op(optMonoid.op(Some(1), None),Some(2)) == optMonoid.op(Some(1), optMonoid.op(None,Some(2))))
 
-  /*
+    //EX 10.5
     def foldMap[A,B](as: List[A], m: Monoid[B])(f: A => B): B={
+      as.map(f(_)).foldLeft(m.zero)(m.op)
     }
-  */
+
+
+    // this is quite meaningless example -I am not yet entirely
+    // sure where foldMap would be useful
+    val ss=
+    foldMap[Int,Double](List(1,2,3,4), new Monoid[Double] {
+      // the operation op should satisfy the associativity i.e. op(op(a,b),c) = op(a,op(b,c))
+      def op(a1: Double, a2: Double): Double = a1 + a2
+
+      // zero or identity function should satisfy op(a,zero) == a
+      def zero: Double = 0
+    })(_.toDouble)
+
+    require(ss==10.0)
+
+   /* EX 10.6 ...
+     def foldLeftViaFoldMap[A,B](zero:B)(f: A=>B):B={
+
+    }
+    */
+
+
+    // EX 10.7 -balanced foldMap
+    def foldMap[A,B](as: IndexedSeq[A], m: Monoid[B])(f: A => B): B={
+
+    }
+
+
+
+
+
 
 
   }
