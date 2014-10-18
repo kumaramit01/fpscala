ce71063f1f590b7236a5e7a03d3a2157b610ed2b
Amit Kumar <akumar2@pandora.com>
2014-10-17 23:37:42 -0700

    fixed broken build

--- src/main/scala/scalazExamples/IterateeExample.scala
+++ src/main/scala/scalazExamples/IterateeExample.scala
@@ -8,11 +8,7 @@ object IterateeExample {
   import scalaz._, Scalaz._, MonadPartialOrder._
   import iteratee._, Iteratee._
   import effect._
-  val stream123 = enumStream[Int, Id](Stream(1, 2, 3))
-  ((head[Int, Id] &= stream123).run) assert_=== Some(1)
-  ((length[Int, Id] &= stream123).run) assert_=== 3
-  ((peek[Int, Id] &= stream123).run) assert_=== Some(1)
-  ((head[Int, Id] &= enumStream(Stream())).run) assert_=== None
+
 
 
 }
