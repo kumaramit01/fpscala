bae76ffe706ca952be759749aae835b6fb5e4001
Amit Kumar <akumar2@pandora.com>
2014-10-30 06:54:55 -0700

    monads -wip

--- src/main/scala/reactive/Monads.scala
+++ src/main/scala/reactive/Monads.scala
@@ -1,5 +1,7 @@
 package reactive
 
+import scala.util.control.NonFatal
+
 /**
  * Data structures with map and flatmap togather with some algebraic laws
  */
@@ -15,5 +17,40 @@ object Monads {
     def unit[T](x:T):M[T]
     def map[U](f:T=>U):M[U]= flatMap(f andThen unit)
   }
+
+  /*
+  abstract class Option[+T]{
+
+    def flatMap[U](f:T=>Option[U]):Option[U]={
+      this match {
+        case None => None
+        case Some(x) => f(x)
+      }
+    }
+  }
+   */
+
+  // significance of the law
+
+  // Associative law allows you to inline the for generator
+  // Right unit law says for(x <- m) yield x == m
+
+
+  /*
+  abstract class Try[+T]
+  case class Success[T](x:T) extends Try[T]
+  case class Failure(ex:Exception) extends Try[Nothing]
+
+  object Try{
+    def apply[T](expr: => T):Try[T]=
+    try Success(expr)
+    catch {
+      case NonFatal(ex) => Failure(ex)
+    }
+  }
+
+*/
+
+
 
 }
